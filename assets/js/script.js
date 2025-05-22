64ba30c4a64182e8a17111c854daf1ec230585cb
MathiasNielsen99 <150657702+MathiasNielsen99@users.noreply.github.com>
2025-05-22 12:06:48 +0200

    Implement initial password generation functionality and update HTML title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log('Weee! I am a JavaScript file!');
+// Random password generator
+
+// Function to generate a random password
+function generatePassword(length, includeUppercase, includeNumbers, includeSymbols, includeLowercase) {
+
+  return '';
+}
+
+const passwordLength = 12;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+const includeLowercase = true;
+
+const password = generatePassword(passwordLength, includeUppercase, includeNumbers, includeSymbols, includeLowercase);
